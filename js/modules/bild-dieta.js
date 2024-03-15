6d3514c4cc9b530a78e74963952a841ea542d7a6
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2024-03-15 00:06:25 -0300

    evente escolher e calc dieta

--- js/modules/bild-dieta.js
+++ js/modules/bild-dieta.js
@@ -1,35 +1,58 @@
 import { buscarAlimentoApi } from "./search-alimento-api.js";
 import { dietaGanharTwin } from "./dieta-ganhar-twin.js";
+const listaDietasImportadas = [dietaGanharTwin];
 
 export function initMinhaDieta(dadosMacros) {
   //display bolck nas dietas ou montar sua dieta
-  //add event form das dieta (click) => ativar biild dieta
-  //bild dieta calcula a dieta
   //print a dieta
 
-  function calcRefeicoes(descricao, proteinas, carboidratos, gorduras) {
+  const dietaMontada = [];
+
+  function bildDieta() {
+    const dietaEscolhida = document.querySelectorAll(".escolha-dieta button");
+    dietaEscolhida.forEach((dieta) => {
+      dieta.addEventListener("click", (event) => {
+        const escolhida = dieta.dataset.dieta;
+        const dietaImportada = listaDietasImportadas[escolhida];
+        if (dietaImportada) {
+          dietaImportada.forEach((ref, i) => {
+            dietaMontada.push(
+              calcRefeicoes(
+                ref.descricao,
+                ref.proteina,
+                ref.carboidrato,
+                ref.gordura
+              )
+            );
+          });
+        }
+      });
+    });
+  }
+  bildDieta();
+
+  const calcRefeicoes = (descricao, proteinas, carboidratos, gorduras) => {
     const refeMontada = [];
     refeMontada.push(descricao);
 
-    if(proteinas){
+    if (proteinas) {
       proteinas.forEach((p) => {
         buscarAlimentoApi(p.description).then((listaEncontrada) => {
           listaEncontrada.forEach((a) => {
-            const percentProtGramas =
-              (dadosMacros.proteina / 100) * p.percent;
+            const percentProtGramas = (dadosMacros.proteina / 100) * p.percent;
             const protAlimPor100g = a.protein_g;
             const gramasDeAlimentoPRefe =
               (percentProtGramas / protAlimPor100g) * 100;
-             refeMontada.push({
+            refeMontada.push({
               gramas: gramasDeAlimentoPRefe.toFixed(0),
-              alimento: p.description
-             }) 
+              alimento: p.description,
+            });
           });
         });
       });
     }
 
-    if(carboidratos) {
+    if (carboidratos) {
       carboidratos.forEach((c) => {
         buscarAlimentoApi(c.description).then((listaEncontrada) => {
           listaEncontrada.forEach((a) => {
@@ -37,70 +60,50 @@ export function initMinhaDieta(dadosMacros) {
 
             const percentCarbGramasDescanso =
               (dadosMacros.carboDescanso / 100) * c.percent;
-              const percentCarbGramasTreino =
+            const percentCarbGramasTreino =
               (dadosMacros.carboTreino / 100) * c.percent;
-              const percentCarbGramasCardio =
+            const percentCarbGramasCardio =
               (dadosMacros.carboCardio / 100) * c.percent;
-              const percentCarbGramasCardioTreino =
+            const percentCarbGramasCardioTreino =
               (dadosMacros.carboTreinoCardio / 100) * c.percent;
             const gramasDescando =
               (percentCarbGramasDescanso / carbAlimPor100g) * 100;
-              const gramasTreino =
+            const gramasTreino =
               (percentCarbGramasTreino / carbAlimPor100g) * 100;
-              const gramasCardio =
+            const gramasCardio =
               (percentCarbGramasCardio / carbAlimPor100g) * 100;
-              const gramasCardioTreino =
+            const gramasCardioTreino =
               (percentCarbGramasCardioTreino / carbAlimPor100g) * 100;
 
-
-             refeMontada.push({
+            refeMontada.push({
               gramasDescando: gramasDescando.toFixed(0),
               gramasTreino: gramasTreino.toFixed(0),
               gramasCardio: gramasCardio.toFixed(0),
               gramasCardioTreino: gramasCardioTreino.toFixed(0),
 
-              alimento: c.description
-             }) 
+              alimento: c.description,
+            });
           });
         });
       });
     }
 
-    if(gorduras){
+    if (gorduras) {
       gorduras.forEach((g) => {
         buscarAlimentoApi(g.description).then((listaEncontrada) => {
           listaEncontrada.forEach((a) => {
-            const percentGordGramas =
-              (dadosMacros.gordura / 100) * g.percent;
+            const percentGordGramas = (dadosMacros.gordura / 100) * g.percent;
             const gordAlimPor100g = a.lipid_g;
             const gramasDeAlimentoPRefe =
               (percentGordGramas / gordAlimPor100g) * 100;
-             refeMontada.push({
+            refeMontada.push({
               gramas: gramasDeAlimentoPRefe.toFixed(0),
-              alimento: g.description
-             }) 
+              alimento: g.description,
+            });
           });
         });
       });
     }
-
-console.log(refeMontada)
-    return refeMontada
-  }
-
-
-const dietaMontada = [];
-
-  function refe1() {
-    const dietaEscolhida = [...dietaGanharTwin];
-    dietaEscolhida.forEach((ref, i) => {
-      //console.log(ref.gordura);
-      dietaMontada.push(calcRefeicoes(ref.descricao,ref.proteina, ref.carboidrato, ref.gordura)
-      )
-
-
-    });
-  }
-  refe1();
-  console.log(dietaMontada)
+    return refeMontada;
+  };
 }
