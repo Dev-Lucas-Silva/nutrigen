a0ecaf4bc9754a4c97ce48f5dcba6c21741ad0dc
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2024-04-15 22:53:54 -0300

    menu mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 import * as initForm from "./modules/init-form.js";
-
+import MenuMobile from './modules/menu-mobile.js';
 initForm.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
